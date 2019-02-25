72737a24a8367f745294ef08e232febdf20599e1
magechaP <magechapeter@gmail.com>
2019-02-25 11:10:09 +0300

    transfer wrong code in css stylesheet to js scripts

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+var  ans1;
+var  ans2;
+var  ans3;
+var  ans4;
+var  ans5;
+var  ans6;
+var  ans7;
+var  ans8;
+var  ans9;
+var  ans10;
+
+$ ("Questions") .change(function(){
+     ans= $(this).val();
+};

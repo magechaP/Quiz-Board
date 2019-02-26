9bba25e7b7e342772c23d54b436b57ffa40047af
magechaP <magechapeter@gmail.com>
2019-02-26 08:17:23 +0300

    add button to question sheet

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,33 @@ var  ans8;
 var  ans9;
 var  ans10;
 
-$ ("Questions") .change(function(){
-     ans= $(this).val();
+$ ("Question1") .change(function(){
+     ans1= $(this).val();
+};
+$ ("Question2") .change(function(){
+     ans2= $(this).val();
+};
+$ ("Question3") .change(function(){
+     ans3= $(this).val();
+};
+$ ("Question4") .change(function(){
+     ans4= $(this).val();
+};
+$ ("Question5") .change(function(){
+     ans5= $(this).val();
+};
+$ ("Question6") .change(function(){
+     ans6= $(this).val();
+};
+$ ("Question7") .change(function(){
+     ans7= $(this).val();
+};
+$ ("Question8") .change(function(){
+     ans8= $(this).val();
+};
+$ ("Question9") .change(function(){
+     ans9= $(this).val();
+};
+$ ("Question10") .change(function(){
+     ans10= $(this).val();
 };
